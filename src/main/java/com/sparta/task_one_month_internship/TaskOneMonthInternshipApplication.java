3db5792c3b53ed7bda5ff2f01480642f151f7ffd
DAchoMI <dachomi97@gmail.com>
2024-08-07 20:33:58 +0900

    :tada: Begin Project

---
+++ src/main/java/com/sparta/task_one_month_internship/TaskOneMonthInternshipApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.task_one_month_internship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskOneMonthInternshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskOneMonthInternshipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/task_one_month_internship/TaskOneMonthInternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.task_one_month_internship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskOneMonthInternshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
